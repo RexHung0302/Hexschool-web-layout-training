8facce9172a2f1da04a98297a4083a01971e4ec7
RexHung <zehung860486@gmail.com>
2020-05-12 02:29:30 +0800

    Update Week_5 & fix README.md

---
+++ Week_5/README.md
@@ -0,0 +1,72 @@
+# Week_5 - 後台表單設計
+
+## 簡介
+
+經過 **RWD** 的地獄式摧殘之後終於來一堂比較輕鬆一點點的後台版面，此次切版挑戰不使用任何 **Bootstrap**。
+
+---
+
+## 關卡目標
+
+1. Bootstrap 4 套用
+
+2. 後台表單設計
+
+---
+
+## 心得
+
+這週開始換到後台的版面了，由於平常就會使用 **Bootstrap**，所以這次就決定要手幹 **Modal** 彈窗以及 **Table**。
+
+本週少了 **RWD** 對於切版上比較輕鬆一點，但是這週切版花比較多時間在 **CSS** 及 **JS** 上面。
+
+另外這次也是使用了 **Gulp** 來切版，因為還是有多頁面的問題，現在直接上癮 **Gulp** 了哈！
+
+> *這次作業有預留了首頁(**Dashboard**)，所以 **Demo** 網址結尾會是 **assignment.html**！*
+
+---
+
+## 安裝步驟
+
+```
+//- 安裝 node.js 跳過
+
+1. $ npm install --global gulp //- 安裝全域 gulp, 如果無法安裝請在最前方加上 sudo -> 例：sudo npm i gulp -g
+
+2. $ git clone 
+
+3. $ cd Hexschool-web-layout-training 
+
+4. $ cd Week_5 // 本次作業在第五週
+
+5. $ npm install // 安裝套件
+
+6. $ gulp // 輸入 gulp 後面如果沒帶名稱會自動抓 gulp 的 default 任務(Gulp 介紹請點參考下方連結處)
+
+7. Enjoy!
+
+```
+
+---
+
+## 連結
+
+Source Code：[點我](https://github.com/RexHung0302/Hexschool-web-layout-training/tree/master/Week_5)
+
+設計稿：[點我](https://xd.adobe.com/view/bd869667-ead5-4620-4329-ee0709cfef9e-cbb7/)
+
+Demo：[點我](https://rexhung0302.github.io/Hexschool-web-layout-training/Week_5/dist/assignment.html)
+
+Blog: [點我](https://rexhung0302.github.io/2020/05/05/20200505/#mor)
+
+> Gulp 介紹：[點我](https://rexhung0302.github.io/2020/05/06/20200506/#more)
+
+---
+
+本週頁面目錄在此：
+
+1. [首頁](https://rexhung0302.github.io/Hexschool-web-layout-training/Week_5/dist)
+
+2. [Assignment](https://rexhung0302.github.io/Hexschool-web-layout-training/Week_5/dist/assignment.html)
+
+3. [Admin](https://rexhung0302.github.io/Hexschool-web-layout-training/Week_5/dist/admin.html)
---
+++ Week_5/app/assets/js/all.js
@@ -0,0 +1,130 @@
+let userRow = document.getElementsByClassName('userRow');
+
+//- 判斷目前頁面
+$(document).ready(function(){
+  // alert(location.pathname);
+  // 目前在 Admin 頁面
+  if(location.pathname === '/admin.html') {
+    $('#Admin_li').addClass('active');
+  }
+  // 目前在 Assignment 頁面
+  else if(location.pathname === '/assignment.html') {
+    $('#Assignment_li').addClass('active');
+  }
+  // 其他
+  else {
+    $('#Dashboard_li').addClass('active');
+  }
+});
+
+//- 點擊打開回覆框
+$('#outside_reply').click(function(){
+  // let contentInfo_width = $('#contentInfo').width();
+  let userRow_height = $('.userRow').height();
+  //- 加入 show Class name
+  $('#messageBox').addClass('show');
+  //- 加入寬度高度
+  $('#replyBox').css('width', '100%');
+  $('#replyBox').css('height', userRow_height + 32 + 36);
+});
+
+//- 點擊關閉回覆框
+$('#cancelBtn').click(function(){
+  //- 移除 show Class name
+  $('#messageBox').removeClass('show');
+  //- 移除寬度高度
+  $('#replyBox').css('width', '0%');
+  $('#replyBox').css('height', '0');
+
+  //- 清空輸入框框內容(使用 empty 因為是 Div 不是真的 Input)
+  $('#contenteditableBox').empty('');
+});
+
+//- 點擊圖片上傳
+$('#imgUploadBtn').click(function(){
+  //- 先清空 input
+  $('#imgUploadInput').val('');
+  //- 點擊隱藏的 Input 按鍵
+  $('#imgUploadInput').click();
+});
+
+//- 圖片上傳
+$('#imgUploadInput').change(function(){
+  //- 檔案
+  let file = $('#imgUploadInput').prop('files')[0];
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+  reader.onload = function (e) {
+    // 創造新標籤
+    let imgElement = document.createElement('img');
+    // 取得圖片路徑
+    let imgUrl = e.target.result;
+    // 取得新的 ID
+    let nweId = new Date(); 
+    // 加入 ID
+    imgElement.setAttribute('id', nweId);
+    // 加入圖片路徑
+    imgElement.setAttribute('src', imgUrl);
+    // 更改圖片大小
+    imgElement.setAttribute('width', '200px');
+    imgElement.setAttribute('height', '200px');
+    // 插入圖片
+    $('#contenteditableBox').append(imgElement);
+  }
+});
+
+//- 點擊檔案上傳
+$('#fileUploadBtn').click(function(){
+  //- 先清空 input
+  $('#fileUploadInput').val('');
+  //- 點擊隱藏的 Input 按鍵
+  $('#fileUploadInput').click();
+});
+
+////////////////* 以下為 Admin 頁面 *////////////////
+
+//- 顯示個人詳細資訊彈窗
+function showDetailModalHandler(){
+  $('#detailModal').addClass('modal-show');
+};
+
+//- 顯示新增個人詳細資訊彈窗
+function showAddDetailModalHandler() {
+  $('#addDetailModal').addClass('modal-show');
+};
+
+//- 顯示個人編輯詳細資訊彈窗
+function showEditDetailModalHandler(){
+  //- 設定角色的 Value
+  $('#accessLevel').val('1');
+  $('#editDetailModal').addClass('modal-show');
+};
+
+//- 關閉個人詳細資訊彈窗 或 關閉新增個人詳細資訊彈窗 或 關閉個人編輯詳細資訊彈窗
+$('.closeModal-btn').click(function(){
+  $('#detailModal').removeClass('modal-show');
+  $('#addDetailModal').removeClass('modal-show');
+  $('#editDetailModal').removeClass('modal-show');
+});
+
+//- 顯示個人詳細按鈕
+$('#showDetailBtn').click(function(){
+  //- 加上 show 顯示按鈕
+  $('#detailBtnInfo').toggleClass('show');
+  //- 加上 show 讓箭頭跑動畫
+  $('#showDetailRow__arrowIcon').toggleClass('show');
+});
+
+//- 點擊空白區域外
+$(document).mouseup(function(e){
+  let clickTargetArea = $(' #targetOne');    // 設定點擊目標區域
+  let clickTargetArea_2 = $('#targetTwo');   // 設定點擊目標區域
+  let clickTargetArea_3 = $('#targetThree');   // 設定點擊目標區域
+  if(!clickTargetArea.is(e.target) && clickTargetArea.has(e.target).length === 0 && !clickTargetArea_2.is(e.target) && clickTargetArea_2.has(e.target).length === 0 && !clickTargetArea_3.is(e.target) && clickTargetArea_3.has(e.target).length === 0) {
+    //- 關閉個人詳細資訊彈窗 或 關閉新增個人詳細資訊彈窗 或 關閉個人編輯詳細資訊彈窗
+    $('#detailModal').removeClass('modal-show');
+    $('#addDetailModal').removeClass('modal-show');
+    $('#editDetailModal').removeClass('modal-show');
+  }
+});
